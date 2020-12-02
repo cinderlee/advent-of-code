1f05449c402c2fda7f132407a1740141c25b7272
Cindy Lee <clee@squarespace.com>
2020-12-02 17:18:13 -0500

    adding breaks

--- day1/day1part2.py
+++ day1/day1part2.py
@@ -5,6 +5,8 @@ lst = file.read().split('\n')
 for index in range(len(lst)):
     lst[index] = int(lst[index])
 
+found = False
+
 for index in range(len(lst)):
     first_num = lst[index]
     next_diff = 2020 - first_num
@@ -12,6 +14,10 @@ for index in range(len(lst)):
     for index2 in range(index + 1, len(lst)):
         if next_diff - lst[index2] in lst:
             print(first_num * lst[index2] * (next_diff - lst[index2]))
+            found = True
             break
+
+    if found:
+        break
 
 file.close()
