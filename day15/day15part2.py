35439532de431d295380947132e19a4f0ebe3d5d
Cindy Lee <clee@squarespace.com>
2021-01-01 13:19:55 -0500

    Refactoring

--- day15/day15part2.py
+++ day15/day15part2.py
@@ -1,35 +1,53 @@
-# file = open('day15testinput.txt')
-file = open('day15input.txt')
+# FILE_NM = 'day15testinput.txt'
+FILE_NM = 'day15input.txt'
+LAST_TURN = 30000000
 
-last_num = None
-curr_num = None
-spoken = {}
-turn = 0
-for line in file:
-    line = line.strip('\n').split(',')
-    for elem in line:
-        if curr_num is None:
-            curr_num = int(elem)
+
+def read_file(file_nm):
+    '''
+    Returns:
+        curr_num: number to be spoken for the next turn
+        spoke_numbers: a dictionary of numbers mapped to the last turn they were said
+    '''
+    file = open(FILE_NM, 'r')
+    prev_num = None
+    curr_num = None
+    spoken_numbers = {}
+    turn = 0
+
+    for line in file:
+        line = line.strip('\n').split(',')
+        for elem in line:
+            if curr_num is None:
+                curr_num = int(elem)
+            else:
+                prev_num = curr_num
+                curr_num = int(elem)
+                spoken_numbers[prev_num] = turn
+            turn += 1
+
+    file.close()
+    return curr_num, spoken_numbers
+
+def get_nth_number(curr_num, spoken_numbers_dict, n):
+    turn = len(spoken_numbers_dict) + 1
+
+    while turn < n:
+        if curr_num not in spoken_numbers_dict:
+            spoken_numbers_dict[curr_num] = turn
+            curr_num = 0
         else:
-            last_num = curr_num
-            curr_num = int(elem)
-            spoken[last_num] = turn
+            last_turn_spoken = spoken_numbers_dict[curr_num]
+            spoken_numbers_dict[curr_num] = turn 
+            curr_num = turn - last_turn_spoken
 
         turn += 1
 
-file.close()
+    return curr_num
 
-# only difference between part1 and part2 is the turn number
-while turn < 30000000:
-    if curr_num not in spoken:
-        spoken[curr_num] = turn
-        curr_num = 0
-    else:
-        last_turn_spoken = spoken[curr_num]
-        spoken[curr_num] = turn 
-        curr_num = turn - last_turn_spoken
+def main():
+    curr_num, spoken_numbers = read_file(FILE_NM)
+    last_num = get_nth_number(curr_num, spoken_numbers, LAST_TURN)
+    print(last_num)
 
-    turn += 1
-
-print(curr_num)
-
+main()
