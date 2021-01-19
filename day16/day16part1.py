bc9d0295ded5c682bd52aaa34a24a9f54575299b
Cindy Lee <leecindy405@gmail.com>
2021-01-19 00:01:38 -0500

    Addig function details and starting to refactor day16

--- day16/day16part1.py
+++ day16/day16part1.py
@@ -1,6 +1,36 @@
 # file = open('day16testinput.txt')
 file = open('day16input.txt')
 
+def read_file(file_nm):
+    rules_section = True
+    your_ticket_section = False
+    nearby_tickets_section = False
+
+    file = open(file_nm, 'r')
+    for line in file:
+        line = line.strip('\n')
+        if not line:
+            continue
+        if rules_section:
+            line = line.split(': ')
+            val_string = line[1].split(' or ')
+
+            part1 = val_string[0]
+            add_valid_vals(part1, valid_nums)
+
+            part2 = val_string[1]
+            add_valid_vals(part2, valid_nums)
+        elif 'your ticket' in line:
+            your_ticket = True
+        elif 'nearby tickets' in line:
+            nearby_tickets = True
+            your_ticket = False
+        elif your_ticket:
+            continue
+        elif nearby_tickets:
+            add_invalid_nums(line, valid_nums, invalid_nums)
+    file.close()
+
 valid_nums = set()
 invalid_nums = []
 
--- day25/day25part1.py
+++ day25/day25part1.py
@@ -5,6 +5,16 @@ DOOR_PUB_KEY = 6269621
 INITIAL_SUBJECT = 7
 
 def get_loop_size(pub_key):
+    '''
+    The handshake operation transforms a subject number. 
+    Steps:
+        start at value of 1
+        repeat number of times (loop size):
+            set the value to itself multiplied by subject number
+            set the value to remainder from dividing by 20201227
+
+    Returns the loop size given a public key
+    '''
     value = 1
     loop_size = 0
     while value != pub_key:
@@ -13,12 +23,22 @@ def get_loop_size(pub_key):
     return loop_size
 
 def get_encryption_key(loop_size, pub_key):
+    '''
+    Returns the encryption key given a loop size and a public key.
+
+    Note:
+    The card transforms the door's public key using the card's loop size.
+    The door transforms the card's public key using the door's loop size.
+    '''
     encr_key = 1
     for i in range(loop_size):
         encr_key = (encr_key * pub_key) % 20201227
     return encr_key
 
 def solve(card_pub_key, door_pub_key):
+    '''
+    Reverse engineer cryptographic handshake and return encryption key.
+    '''
     card_loop_size = get_loop_size(card_pub_key)
     door_loop_size = get_loop_size(door_pub_key)
 
