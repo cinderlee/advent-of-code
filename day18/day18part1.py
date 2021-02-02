6687ff020d0db30b9215b79b11e4db46b1741c63
Cindy Lee <leecindy405@gmail.com>
2021-02-02 00:56:44 -0500

    Refactoring day18

--- day18/day18part1.py
+++ day18/day18part1.py
@@ -1,80 +1,110 @@
-# file = open('day18testinput.txt', 'r')
-file = open('day18input.txt', 'r')
+FILE_TEST_NM = 'day18testinput.txt'
+FILE_NM = 'day18input.txt'
 
-def eval_line(start_index, line):
+def tokenize(expression):
+    '''
+    Returns a list of tokens of the given expression.
+    '''
+    tokens = []
+    start = 0
+    end = 0
+    while start < len(expression):
+        if (start < len(expression) and end == len(expression)) or expression[end] == ' ':
+            token = expression[start:end]
+            if token == '*' or token == '+':
+                tokens.append(token)
+            elif token:
+                tokens.append(int(token))
+            start = end + 1
+            end += 1
+        elif expression[end] == '(':
+            tokens.append(expression[end])
+            start = end + 1
+            end += 1
+        elif expression[end] == ')':
+            if expression[start:end]:
+                tokens.append(int(expression[start:end]))
+            tokens.append(expression[end])
+            start = end + 1
+            end += 1
+        else:
+            end += 1
+    return tokens
+
+def eval_expression(index, token_lst):
+    '''
+    Evaluates the expression or sub-expression given a list 
+    of tokens and a start index.
+
+    Returns the total of the evaluated expression, and the next
+    index to start from.
+    '''
     total = None
     mult = False
     add = False
 
-    start = start_index
-    end = start_index
-    while start < len(line):
-        if end >= len(line):
-            if start < len(line):
-                token = line[start : end]
-                if mult:
-                    total *= int(token)
-                else:
-                    total += int(token)
-            break
+    while index < len(token_lst):
+        token = token_lst[index]
+        if token == '(':
+            num, index = eval_expression(index + 1, token_lst)
 
-        if line[end] == '(':
-            num, next_index = eval_line(end + 1, line)
-            start = next_index
-            end = next_index
-
-            if total is None:
-                total = num
-            elif mult:
+            if mult:
                 total *= num
-            else:
+            elif add:
                 total += num
-
+            else:
+                total = num
             mult = False
             add = False
 
-        elif line[end] == ')':
-            token = line[start : end]
-            if token:
-                if mult:
-                    total *= int(token)
-                else:
-                    total += int(token)
-            return total, end + 1
-
-        elif line[end] == ' ':
-            token = line[start : end]
-            if token:
-                if token == '*':
-                    mult = True
-                elif token == '+':
-                    add = True
-                elif total is None:
-                    total = int(token)
-                elif mult:
-                    total *= int(token)
-                    mult = False
-                else:
-                    total += int(token)
-                    add = False
-            start = end + 1
-            end += 1
+        elif token == ')':
+            return total, index + 1
 
         else:
-            end += 1
+            if token == '*':
+                mult = True
+            elif token == '+':
+                add = True
+            elif mult:
+                total *= int(token)
+                mult = False
+            elif add:
+                total += int(token)
+                add = False
+            else:
+                total = int(token)
+            index += 1
 
-    return total, start
+    return total, index
 
-test = False
+def read_file(file_nm):
+    '''
+    Returns a list of list of tokens for all expressions
+    read from a file.
+    '''
+    file = open(file_nm, 'r')
+    token_exprs = []
+    for line in file: 
+        token_exprs.append(tokenize(line.strip('\n')))
+    file.close()
+    return token_exprs
 
-total = 0 
-for line in file:
-    line = line.strip('\n')
-    if test:
-        print(eval_line(0, line)[0])
-    else:
-        total += eval_line(0, line)[0]
+def get_total(token_exprs_lst):
+    '''
+    Returns the accumulative total of all expressions given 
+    a list of list of tokens. 
+    '''
+    total = 0
+    for token_expr in token_exprs_lst:
+        total += eval_expression(0, token_expr)[0]
+    return total
 
-file.close()
+def solve(file_nm):
+    token_exprs = read_file(file_nm)
+    return get_total(token_exprs)
 
-print(total)
+def main():
+    assert(solve(FILE_TEST_NM) == 25969)
+    print(solve(FILE_NM))
+
+main()
