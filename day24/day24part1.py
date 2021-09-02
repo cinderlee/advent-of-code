1ab4c9271054181ce41c2cc4dcf3ac747e85b096
Cindy Lee <clee@squarespace.com>
2021-09-01 23:03:44 -0400

    adding comments and refactoring

--- day24/day24part1.py
+++ day24/day24part1.py
@@ -1,4 +1,7 @@
-dirs = {
+FILE_TEST_NM = 'day24testinput.txt'
+FILE_NM = 'day24input.txt'
+
+DIRS = {
     'e': (2, 0),
     'w': (-2, 0),
     'nw': (-1, 1),
@@ -8,46 +11,74 @@ dirs = {
 }
 
 def get_loc(input_line):
+    '''
+    A hexagonal tile has 6 neighbors: east, southeast, southwest, west, 
+    northwest, and northeast, which are denoted by e, se, sw, w, nw, 
+    and ne respectively. The tiles are placed in a hex grid. The input
+    line represents a series of steps starting from a reference tile 
+    in the center of the room. 
+
+    Returns the location of the tile.
+    '''
     index = 0
     x = 0
     y = 0
     while index < len(input_line):
         if input_line[index] == 'e' or input_line[index] == 'w':
-            dir_x, dir_y = dirs[input_line[index]]
+            dir_x, dir_y = DIRS[input_line[index]]
             x, y = x + dir_x, y + dir_y
             index += 1
         else:
-            dir_x, dir_y = dirs[input_line[index : index + 2]]
+            dir_x, dir_y = DIRS[input_line[index : index + 2]]
             x, y = x + dir_x, y + dir_y
             index += 2
 
     return x, y
 
 def count_black_tiles(tiles_dict):
-    count_black = 0
-    for elem in tiles_dict:
-        if tiles_dict[elem]:
-            count_black +=1
+    '''
+    Returns the number of tiles with black side up
+    '''
+    count = 0
+    for tile in tiles_dict:
+        if tiles_dict[tile]:
+            count +=1
 
-    return count_black
+    return count
+
+def flip_tiles(file_nm):
+    '''
+    Flips hexagonal tiles from a file that contains a list of tiles 
+    that need to be flipped over. All tiles are initially facing white side up.
+
+    Returns a dictionary of tiles where the key is the tile location
+    and the value determines whether the tile is black side up.
+    '''
+    tiles = {}
+    tiles_file = open(file_nm, 'r')
+    for line in tiles_file:
+        loc = get_loc(line.strip('\n'))
+
+        if loc in tiles:
+            tiles[loc] = not tiles[loc]
+        else:
+            tiles[loc] = True
+
+    tiles_file.close()
+    return tiles
+
+def solve(file_nm):
+    '''
+    Returns the number of black tiles after flipping the tiles listed in a file.
+    '''
+    return count_black_tiles(flip_tiles(file_nm))
+
+def main():
+    assert(solve(FILE_TEST_NM) == 10)
+    print(solve(FILE_NM))
 
 
-file = open('day24input.txt')
-
-tiles = {} 
-
-for line in file:
-    line = line.strip('\n')
-    loc = get_loc(line)
-
-    if loc in tiles:
-        tiles[loc] = not tiles[loc]
-    else:
-        tiles[loc] = True
-
-file.close()
-
-print(count_black_tiles(tiles))
+main()
 
     
 
--- day24/day24part2.py
+++ day24/day24part2.py
@@ -1,4 +1,8 @@
-dirs = {
+NUM_DAYS = 100
+FILE_TEST_NM = 'day24testinput.txt'
+FILE_NM = 'day24input.txt'
+
+DIRS = {
     'e': (2, 0),
     'w': (-2, 0),
     'nw': (-1, 1),
@@ -7,26 +11,56 @@ dirs = {
     'se': (1, -1)
 }
 
-directions = list(dirs.values())
-
 def get_loc(input_line):
+    '''
+    A hexagonal tile has 6 neighbors: east, southeast, southwest, west, 
+    northwest, and northeast, which are denoted by e, se, sw, w, nw, 
+    and ne respectively. The tiles are placed in a hex grid. The input
+    line represents a series of steps starting from a reference tile 
+    in the center of the room. 
+
+    Returns the location of the tile.
+    '''
     index = 0
     x = 0
     y = 0
     while index < len(input_line):
         if input_line[index] == 'e' or input_line[index] == 'w':
-            dir_x, dir_y = dirs[input_line[index]]
+            dir_x, dir_y = DIRS[input_line[index]]
             x, y = x + dir_x, y + dir_y
             index += 1
-
         else:
-            dir_x, dir_y = dirs[input_line[index : index + 2]]
+            dir_x, dir_y = DIRS[input_line[index : index + 2]]
             x, y = x + dir_x, y + dir_y
             index += 2
 
     return x, y
 
+def set_up_tiles(file_nm):
+    '''
+    Sets up the floor of hexagonal tiles from a file that contains a list of tiles 
+    that need to be flipped over. All tiles are initially facing white side up.
+
+    Returns a dictionary of tiles where the key is the tile location
+    and the value determines whether the tile is black side up.
+    '''
+    tiles = {}
+    tiles_file = open(file_nm, 'r')
+    for line in tiles_file:
+        loc = get_loc(line.strip('\n'))
+
+        if loc in tiles:
+            tiles[loc] = not tiles[loc]
+        else:
+            tiles[loc] = True
+
+    tiles_file.close()
+    return tiles
+
 def add_neighbors(tiles_dict):
+    '''
+    Adds tile locations adjacent to black tiles to a tile dictionary.
+    '''
     neighbors = set()
 
     for tile in tiles_dict:
@@ -34,7 +68,7 @@ def add_neighbors(tiles_dict):
             continue
         x, y = tile
 
-        for dir_x, dir_y in directions:
+        for dir_x, dir_y in DIRS.values():
             neighbor_x = x + dir_x
             neighbor_y = y + dir_y
 
@@ -45,28 +79,39 @@ def add_neighbors(tiles_dict):
             tiles_dict[neighbor] = False
 
 def count_black_neighbors(tiles_dict, loc):
+    '''
+    Given a dictionary of tiles and a tile location, returns the number
+    of blakc tiles adjacent to the location.
+    '''
     x, y = loc
-    blacks = 0
+    count = 0
 
-    for dir_x, dir_y in directions:
+    for dir_x, dir_y in DIRS.values():
         neighbor_x = x + dir_x
         neighbor_y = y + dir_y
 
         if (neighbor_x, neighbor_y) in tiles_dict:
             if tiles_dict[(neighbor_x, neighbor_y)]:
-                blacks += 1 
-    return blacks
+                count += 1 
+    return count
 
 def count_black_tiles(tiles_dict):
-    count_black = 0
+    '''
+    Returns the number of tiles with black side up
+    '''
+    count = 0
+    for tile in tiles_dict:
+        if tiles_dict[tile]:
+            count +=1
 
-    for elem in tiles_dict:
-        if tiles_dict[elem]:
-            count_black +=1
-
-    return count_black
+    return count
 
 def flip(tiles_dict):
+    '''
+    Each day, the tiles are flipped according to these rules:
+        A black tile with 0 or more than 2 black tiles adjacent to it is flipped to white.
+        A white tile with exactly 2 black tiles adjacent to it is flipped to black
+    '''
     changes = []
     add_neighbors(tiles_dict)
 
@@ -74,34 +119,31 @@ def flip(tiles_dict):
         black_neighbors = count_black_neighbors(tiles_dict, elem)
 
         if tiles_dict[elem] and (black_neighbors == 0 or black_neighbors > 2):
-            changes.append((elem, False))
+            changes.append((elem))
 
         elif not tiles_dict[elem] and black_neighbors == 2:
-            changes.append((elem, True))
+            changes.append((elem))
 
-    for tile, val in changes:
-        tiles_dict[tile] = val
+    for tile in changes:
+        tiles_dict[tile] = not tiles_dict[tile]
+
+def solve(file_nm, num_days):
+    '''
+    Returns the number of black tiles after flipping a floor of tiles for 
+    a number of days.
+    '''
+    tiles = set_up_tiles(file_nm)
+
+    day = 0 
+    while day < num_days:
+        flip(tiles)
+        day += 1
+
+    return count_black_tiles(tiles)
+
+def main():
+    assert(solve(FILE_TEST_NM, NUM_DAYS) == 2208)
+    print(solve(FILE_NM, NUM_DAYS))
 
 
-file = open('day24input.txt')
-
-tiles = {} 
-
-for line in file:
-    line = line.strip('\n')
-    loc = get_loc(line)
-
-    if loc in tiles:
-        tiles[loc] = not tiles[loc]
-    else:
-        tiles[loc] = True
-
-file.close()
-
-
-day = 0 
-while day < 100:
-    flip(tiles)
-    day += 1
-
-print(count_black_tiles(tiles))
+main()
